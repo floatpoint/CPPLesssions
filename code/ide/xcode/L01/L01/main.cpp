456658e7773cc98c3955fad855ff82aeac9df5fd
floatpoint <chentm1020@icloud.com>
2015-08-31 17:03:56 +0800

    L01
    第一课

---
+++ code/ide/xcode/L01/L01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L01
+//
+//  Created by jeans on 15-8-31.
+//  Copyright (c) 2015年 jeans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
