3e2b39eefada8b5f3f34c1c85661d0a21a188a64
floatpoint <chentm1020@icloud.com>
2015-08-31 17:10:13 +0800

    第二课学习

---
+++ code/ide/xcode/L02/L02/People.cpp
@@ -0,0 +1,9 @@
+//
+//  People.cpp
+//  L02
+//
+//  Created by jeans on 15-8-31.
+//  Copyright (c) 2015年 jeans. All rights reserved.
+//
+
+#include "People.h"
---
+++ code/ide/xcode/L02/L02/People.h
@@ -0,0 +1,14 @@
+//
+//  People.h
+//  L02
+//
+//  Created by jeans on 15-8-31.
+//  Copyright (c) 2015年 jeans. All rights reserved.
+//
+
+#ifndef __L02__People__
+#define __L02__People__
+
+#include <stdio.h>
+
+#endif /* defined(__L02__People__) */
